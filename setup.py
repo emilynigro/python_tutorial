b37339eba2c0f4460480ea6e21332a0ad497c82f
emilyrobin4 <106027604+emilyrobin4@users.noreply.github.com>
2024-05-29 15:13:13 -0600

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="mysci",
+    version="1.0.0",
+    description="A sample package",
+    author="Emily Nigro",
+    author_email="emilynigro04@gmail.com",
+    packages=["mysci"],
+    install_requires=[],
+)

d279c190af9c23f2001b851961725b122fb8b6b1
emilyrobin4 <106027604+emilyrobin4@users.noreply.github.com>
2024-05-29 14:05:42 -0600

    Creating computation module

--- windchillcomp.py
+++ windchillcomp.py
@@ -1,5 +1,6 @@
 from readdata import read_data
 from printing import print_comparison
+from computation import compute_windchill
 
 # Column names and column indices to read
 columns = {'date': 0, 'time': 1, 'tempout': 2, 'windspeed': 7,
@@ -11,17 +12,6 @@ types = {'tempout': float, 'windspeed': float, 'windchill': float}
 # Read data from file
 data = read_data(columns, types=types)
 
-
-# Compute the wind chill temperature
-def compute_windchill(t, v):
-   a = 35.74
-   b = 0.6215
-   c = 35.75
-   d = 0.4275
-
-   v2 = v ** 2
-   wci = a + (b * t) - (c * v2) + (d * t * v2)
-   return wci
 
 # Compute the wind chill factor
 windchill = []
